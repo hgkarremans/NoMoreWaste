f1d9eda87e5a22ffa54e0615debc1f203760ab7e
Hans Gerard Karremans <hg.karremans@student.avans.nl>
2024-10-03 22:32:12 +0200

    get my mealboxes kinda working

--- UI/Controllers/MealBoxController.cs
+++ UI/Controllers/MealBoxController.cs
@@ -1,61 +1,62 @@
-using Application;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
-using NoMoreWaste.Domain.DomainModels;
+    using Application;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using NoMoreWaste.Domain.DomainModels;
 
-namespace UI.Controllers;
+    namespace UI.Controllers;
 
-public class MealBoxController : Controller
-{
-    private readonly IMealBoxRepository _mealBoxRepository;
-    private readonly ICanteenRepository _canteenRepository;
-    private readonly IStudentRepository _studentRepository;
-    private readonly IProductRepository _productRepository;
-    private readonly ICanteenWorkerRepository _canteenWorkerRepository;
-    private readonly UserManager<IdentityUser> _userManager;
-
-
-    public MealBoxController(IMealBoxRepository mealBoxRepository, ICanteenRepository canteenRepository,
-        IStudentRepository studentRepository, IProductRepository productRepository,
-        ICanteenWorkerRepository canteenWorkerRepository, UserManager<IdentityUser> userManager)
+    public class MealBoxController : Controller
     {
-        _mealBoxRepository = mealBoxRepository;
-        _canteenRepository = canteenRepository;
-        _studentRepository = studentRepository;
-        _productRepository = productRepository;
-        _canteenWorkerRepository = canteenWorkerRepository;
-        _userManager = userManager;
-    }
+        private readonly IMealBoxRepository _mealBoxRepository;
+        private readonly ICanteenRepository _canteenRepository;
+        private readonly IStudentRepository _studentRepository;
+        private readonly IProductRepository _productRepository;
+        private readonly ICanteenWorkerRepository _canteenWorkerRepository;
+        private readonly UserManager<IdentityUser> _userManager;
 
-    public async Task<IActionResult> GetMyMealBoxes()
-    {
-        try
-        {
-            var userId = _userManager.GetUserAsync(User).Id;
-            var mealBoxes = await _mealBoxRepository.GetMyMealboxes(userId);
-            return View(mealBoxes);
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e);
-            throw;
-        }
-    }
 
-    public async Task<IActionResult> ReservateMealBox(int mealBoxId)
-    {
-        try
+        public MealBoxController(IMealBoxRepository mealBoxRepository, ICanteenRepository canteenRepository,
+            IStudentRepository studentRepository, IProductRepository productRepository,
+            ICanteenWorkerRepository canteenWorkerRepository, UserManager<IdentityUser> userManager)
         {
-            var userIdentity = await _userManager.GetUserAsync(User);
-            var user = _studentRepository.GetByEmailAsync(userIdentity.Email);
-            var mealBox = _mealBoxRepository.ReservateMealBoxAsync(mealBoxId, user.Id);
-            TempData["SuccessMessage"] = "Meal box reserved successfully!";
-            return RedirectToAction("Index", "home");
+            _mealBoxRepository = mealBoxRepository;
+            _canteenRepository = canteenRepository;
+            _studentRepository = studentRepository;
+            _productRepository = productRepository;
+            _canteenWorkerRepository = canteenWorkerRepository;
+            _userManager = userManager;
         }
-        catch (Exception e)
+
+        public async Task<IActionResult> GetMyMealBoxes()
         {
-            return BadRequest(e.Message);
+            try
+            {
+                var userIdentity = await _userManager.GetUserAsync(User);
+                var user = _studentRepository.GetByEmailAsync(userIdentity.Email);
+                var mealBoxes = await _mealBoxRepository.GetMyMealboxes(user.Id);
+                return View("MyMealboxes", mealBoxes);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        public async Task<IActionResult> ReservateMealBox(int mealBoxId)
+        {
+            try
+            {
+                var userIdentity = await _userManager.GetUserAsync(User);
+                var user = _studentRepository.GetByEmailAsync(userIdentity.Email);
+                var mealBox = _mealBoxRepository.ReservateMealBoxAsync(mealBoxId, user.Id);
+                TempData["SuccessMessage"] = "Meal box reserved successfully!";
+                return RedirectToAction("Index", "home");
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
         }
     }
-}
