5c56d1533a684bbcfd75c2a23a6f4443d2d130e1
Hans Gerard Karremans <hg.karremans@student.avans.nl>
2024-10-13 22:06:30 +0200

    first working test

--- Tests/CanteenRepositoryTest.cs
+++ Tests/CanteenRepositoryTest.cs
@@ -6,6 +6,7 @@ using Infrastructure.ContextClasses;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
 using NoMoreWaste.Domain.DomainModels;
+using NoMoreWaste.Domain.DomainModels.Enums;
 using NSubstitute;
 using Xunit;
 
@@ -23,10 +24,7 @@ namespace Tests
                 .UseInMemoryDatabase(databaseName: "TestDatabase")
                 .Options;
 
-            _mockContext = Substitute.For<ApplicationDbContext>(options);
-            _mockCanteenSet = Substitute.For<DbSet<Canteen>>();
-
-            // Setup the repository
+            _mockContext = new ApplicationDbContext(options);
             _repository = new CanteenRepository(_mockContext);
         }
 
@@ -34,17 +32,9 @@ namespace Tests
         public async Task GetByIdAsync_ShouldReturnCanteen_WhenCanteenExists()
         {
             // Arrange
-            var canteen = new Canteen { Id = 1, Name = "Main Canteen" };
-            var data = new List<Canteen> { canteen }.AsQueryable();
-
-            // Setup mock DbSet
-            var queryableCanteenSet = (IQueryable<Canteen>)_mockCanteenSet;
-            queryableCanteenSet.Provider.Returns(data.Provider);
-            queryableCanteenSet.Expression.Returns(data.Expression);
-            queryableCanteenSet.ElementType.Returns(data.ElementType);
-            queryableCanteenSet.GetEnumerator().Returns(data.GetEnumerator());
-
-            _mockContext.Canteens.Returns(_mockCanteenSet);
+            var canteen = new Canteen { Id = 1, Name = "Main Canteen", Address = "lovensdijkstraat 32", City = City.Amsterdam, IsWarmFood = false};
+            _mockContext.Canteens.Add(canteen);
+            _mockContext.SaveChanges();
 
             // Act
             var result = await _repository.GetByIdAsync(1);
@@ -54,37 +44,5 @@ namespace Tests
             Assert.Equal(1, result.Id);
             Assert.Equal("Main Canteen", result.Name);
         }
-
-        [Fact]
-        public async Task GetByIdAsync_ShouldThrowException_WhenCanteenNotFound()
-        {
-            // Arrange
-            _mockContext.Canteens.Returns(_mockCanteenSet);
-
-            // Act & Assert
-            await Assert.ThrowsAsync<KeyNotFoundException>(() => _repository.GetByIdAsync(99));
-        }
-
-        [Fact]
-        public async Task CreateAsync_ShouldAddNewCanteen()
-        {
-            // Arrange
-            var canteen = new Canteen { Id = 2, Name = "New Canteen" };
-            var mockEntityEntry = Substitute.For<EntityEntry<Canteen>>();
-
-            // Setup mock for AddAsync
-            _mockCanteenSet.AddAsync(canteen, default).Returns(new ValueTask<EntityEntry<Canteen>>(mockEntityEntry));
-            _mockContext.Canteens.Returns(_mockCanteenSet);
-
-            // Act
-            var result = await _repository.CreateAsync(canteen);
-
-            // Assert
-            await _mockCanteenSet.Received(1).AddAsync(canteen, default);
-            await _mockContext.Received(1).SaveChangesAsync(default);
-            Assert.Equal(canteen, result);
-        }
-
-        // Similarly, you can write tests for UpdateAsync, DeleteAsync, and GetAllAsync
     }
 }
