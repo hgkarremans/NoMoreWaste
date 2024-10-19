3b8d244726f5fdda6f1122f727f0fb393b2a7f7d
hgkarremans <h.karremans@pararius.nl>
2024-10-19 16:18:37 +0200

    added test

--- Tests/RepositoryTest/MealboxRepositoryTest.cs
+++ Tests/RepositoryTest/MealboxRepositoryTest.cs
@@ -1,6 +1,9 @@
+using System.Collections.ObjectModel;
 using Application.Repositories;
 using Infrastructure.ContextClasses;
 using Microsoft.EntityFrameworkCore;
+using NoMoreWaste.Domain.DomainModels;
+using NoMoreWaste.Domain.DomainModels.Enums;
 
 namespace Tests;
 
@@ -24,6 +27,35 @@ public class MealboxRepositoryTest : IAsyncLifetime
     public async Task DisposeAsync()
     {
         await _mockContext.DisposeAsync();
+    }
+    
+    public async Task GetAllAsync_ShouldReturnAllMealboxes_WhenMealboxesExist()
+    {
+        // Arrange
+        var canteen = new Canteen
+        {
+            Id = 1, Name = "Main Canteen", Address = "lovensdijkstraat 32", City = City.Amsterdam, IsWarmFood = false
+        };
+        var product = new Product
+        {
+            Id = 1, Name = "Apple", HasAlcohol = false, ImageUrl = "apple.jpg"
+        };
+        
+        var mealbox1 = new MealBox() {
+        Id = 1, Name = "mealbox1", Canteen = canteen, CanteenId = 1, City = City.Amsterdam, Price = 10m,
+        Products = new Collection<Product>(), EighteenPlus = false, ExpireDate = DateTime.Today, PickUpDate = DateTime.Today, ReservedStudent = 
+        }
+            
+        _mockContext.MealBoxes.Add(mealbox1);
+        _mockContext.MealBoxes.Add(mealbox2);
+        _mockContext.SaveChanges();
+
+        // Act
+        var result = await _repository.GetAllAsync();
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(2, result.Count);
     }
     
 }
