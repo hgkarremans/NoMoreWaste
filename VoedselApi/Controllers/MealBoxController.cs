b928000b8c07c09cdf3f1c0fcc7753d3181654b4
hgkarremans <h.karremans@pararius.nl>
2024-10-23 18:28:24 +0200

    most of the api

---
+++ VoedselApi/Controllers/MealBoxController.cs
@@ -0,0 +1,42 @@
+using Application;
+using Microsoft.AspNetCore.Mvc;
+using NoMoreWaste.Domain.DomainModels;
+
+namespace VoedselApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class MealBoxController
+{
+    private readonly IMealBoxRepository _mealBoxRepository;
+
+    public MealBoxController(IMealBoxRepository mealBoxRepository)
+    {
+        _mealBoxRepository = mealBoxRepository;
+    }
+
+    [HttpGet]
+    public IEnumerable<MealBox> GetMealBoxes()
+    {
+        return _mealBoxRepository.GetAllAsync().Result;
+    }
+    
+    [HttpGet("{id}", Name = "GetMealBox")]
+    public MealBox GetMealBox([FromRoute]int id)
+    {
+        return _mealBoxRepository.GetByIdAsync(id).Result;
+    }
+    // POST: api/MealBox
+    
+    [HttpDelete("{id}", Name="DeleteMealBox")]
+    public void DeleteMealBox([FromRoute]int id)
+    {
+        throw new NotImplementedException();
+    }
+    [HttpPut("{id}", Name="UpdateMealBox")]
+    public void UpdateMealBox([FromBody] MealBox mealBox)
+    {
+        throw new NotImplementedException();
+    }
+    
+}
